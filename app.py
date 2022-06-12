73549389e8a5d6bfd7815f6a1adb6e51016b4d28
Aadityansha Patel <97668502+Aadityansha@users.noreply.github.com>
2022-06-12 11:02:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,10 @@ voices = engine.getProperty('voices')
 engine.setProperty(rate, 160)
 engine.setProperty('voice', voices[1].id)
 
+file = 'periodic-classification-of-elements.pdf'
+
 # reading PDF page by page
-with open('periodic-classification-of-elements.pdf', mode='rb') as book:
+with open(file, mode='rb') as book:
     reader = PyPDF2.PdfFileReader(book)
     page_no = 0
     for page in reader.pages:
